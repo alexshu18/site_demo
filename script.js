b1cc7e0c427c376ed11f69f8d6b1e9fe97c57062
alexshu18 <redsanch11@gmail.com>
2025-09-20 15:11:22 +0300

    Burger margin

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.nav-links');
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
 
-burger.addEventListener('click', () => {
-    nav.classList.toggle('active');
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
 });
